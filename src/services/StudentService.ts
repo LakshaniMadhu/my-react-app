328a123adb4fdedee53ae2b5f54fd280f246f045
shalika00010 <shalika.harshaniwm@gmail.com>
2025-02-28 01:17:46 +0530

    studentservices

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -1,6 +1,24 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8080/api/students'; // Replace with your backend API URL
+const API_URL = "http://localhost:8080/api/students"; // Replace with yourbackend API URL
 export const getStudents = async () => {
  const response = await axios.get(API_URL);
+ return response.data;
+};
+export const addStudent = async (student: {id:string; firstName: string;
+lastName: string; email: string }) => {
+ const response = await axios.post(API_URL, student);
+ return response.data;
+};
+export const getStudentById = async (id: number) => {
+ const response = await axios.get(`${API_URL}/${id}`);
+ return response.data;
+};
+export const updateStudent = async (id: number, student: { firstName:
+string; lastName: string; email: string }) => {
+ const response = await axios.put(`${API_URL}/${id}`, student);
+ return response.data;
+};
+export const deleteStudent = async (id: number) => {
+ const response = await axios.delete(`${API_URL}/${id}`);
  return response.data;
 };
